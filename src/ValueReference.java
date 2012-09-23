dc0cac975f1c8e2951f26b2e42a242eb109ec425
dfarley <student@Habitat.(none)>
2012-09-23 18:08:28 -0400

    added some more code

--- src/ValueReference.java
+++ src/ValueReference.java
@@ -2,7 +2,7 @@
 public class ValueReference {
 
 	public static void main(String[] args) {
-		testAssignment();
+		testMethodParameters();
 	}
 
 	static void testAssignment() {
@@ -12,7 +12,7 @@ public class ValueReference {
 		j = i;
 		j = 22;
 		System.out.println("i: "+i);//prints 'i: 10'
-		System.out.println("j: "+i);//prints 'j: 22'
+		System.out.println("j: "+j);//prints 'j: 22'
 
 		//by reference
 		java.lang.StringBuffer stringBuffer1 = new java.lang.StringBuffer("the original value");
@@ -22,6 +22,25 @@ public class ValueReference {
 		stringBuffer2.append(", and some new text");
 		System.out.println("stringBuffer1: \""+stringBuffer1+"\"");//prints 'stringBuffer1: "the original value", and some new text'
 		System.out.println("stringBuffer2: \""+stringBuffer2+"\"");//prints 'stringBuffer2: "the original value", and some new text'
+	}
+	
+	static void testMethodParameters() {
+		int i = 17;
+		System.out.println("i: "+i); //prints 'i: 17'
+		changeParameter(i);
+		System.out.println("i: "+i); //prints 'i: 17'
 		
+		java.lang.StringBuffer stringBuffer = new java.lang.StringBuffer("the original value");
+		System.out.println("stringBuffer: "+stringBuffer);//prints "the original value"
+		changeParameter(stringBuffer);
+		System.out.println("stringBuffer: "+stringBuffer);//prints "eulav lanigiro eht":!
+	}
+	
+	static void changeParameter(int i) {
+		i = i * 324;
+	}
+	
+	static void changeParameter(java.lang.StringBuffer stringBuffer) {
+		stringBuffer.reverse();
 	}
 }
